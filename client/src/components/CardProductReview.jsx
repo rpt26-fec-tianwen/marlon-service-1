368f0730c764554c27bb114462abfe267f42e56b
Marlon Esparza <marlonaesparza@gmail.com>
2021-03-17 17:52:47 -0700

    Card-Stars are now rendering according to state
    -Additionally, removed console.logs from App.jsx
    -Removed reference to to CSS from index.html
    -NOTE: may not reference on pull request merge, and
    -will make sure to make the same pages on master.

--- client/src/components/CardProductReview.jsx
+++ client/src/components/CardProductReview.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 
 const CardProductReview = (props) => {
   let stars = [];
-  let rating = 0;
 
-  for (let i = 0; i < rating; i++) {
-    let solidStar = <i className="fas fa-star"></i>;
+  for (let i = 0; i < props.rating; i++) {
+    let solidStar = <i key={i} className="fas fa-star"></i>;
     stars.push(solidStar);
   }
 
-  for (let i = 0; i < Math.abs(rating - 5); i++) {
-    let blankStar = <i className="far fa-star"></i>;
+  for (let i = 0; i < Math.abs(props.rating - 5); i++) {
+    let blankStar = <i key={i} className="far fa-star"></i>;
     stars.push(blankStar);
   }
 
   return (
     <div className='card-contents-review'>
-      {stars}<span className='star-rating'>({rating})</span>
+      {stars}<span className='star-rating'>({props.rating})</span>
     </div>
   );
 };
