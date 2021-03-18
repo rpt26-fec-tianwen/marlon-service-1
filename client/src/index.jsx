84da4f30db351a9f2879eb871a9a4a63e1596d00
Marlon Esparza <marlonaesparza@gmail.com>
2021-03-18 15:05:56 -0700

    Updated divs render id to card-content

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,5 +4,5 @@ import App from './components/App.jsx'
 import "./styles/index.css"
 import {getProduct, getProductType, getProductReview} from './utils/axiosRequests.js'
 
-const root = document.getElementById('root');
+const root = document.getElementById('card-content');
 ReactDOM.render(<App getProduct={getProduct} getProductType={getProductType} getProductReview={getProductReview}/>, root);
