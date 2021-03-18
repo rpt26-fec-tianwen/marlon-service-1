415558673d0517a6e06adb9cdc808d377071ff56
Marlon Esparza <marlonaesparza@gmail.com>
2021-03-18 12:09:09 -0700

    Created/Updated request helpers
    -Each request helper function now contains:
    a. Params for proxy server to examine
    b. Notes so you know how to interact with my helpers

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx'
 import "./styles/index.css"
-import {getProduct} from './utils/axiosRequests.js'
+import {getProduct, getProductType, getProductReview} from './utils/axiosRequests.js'
 
 const root = document.getElementById('root');
-ReactDOM.render(<App getProduct={getProduct}/>, root);
+ReactDOM.render(<App getProduct={getProduct} getProductType={getProductType} getProductReview={getProductReview}/>, root);
--- client/src/utils/axiosRequests.js
+++ client/src/utils/axiosRequests.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
 export const getProduct = (id) => {
-  let path = '/card/' + id;
+  // comment out "path" in proxy
+  // reference window.location.href instead
+  const path = '/card/' + id;
 
-  let config = {
+  // use these in proxy
+  // will guide your request
+  const config = {
     params: {
       service: 'card',
       host: 'localhost',
@@ -18,3 +22,52 @@ export const getProduct = (id) => {
       return response.data;
     });
 };
+
+
+export const getProductType = (id) => {
+  // comment out "path" in proxy
+  // reference window.location.href instead
+  const path = '/productDetail/activity/' + id;
+
+  // use these in proxy
+  // will guide your request
+  const config = {
+    params: {
+      service: 'details',
+      host: 'localhost',
+      port: 8003,
+      path: '/',
+      productId: id,
+      indicator: 'activity'
+    }
+  };
+
+  return axios.get(path, config)
+    .then((response) => {
+      return response.data;
+    });
+};
+
+
+export const getProductReview = (id) => {
+  // comment out "path" in proxy
+  // reference window.location.href instead
+  const path =  '/reviews-products/' + id;
+
+  // use these in proxy
+  // will guide your request
+  const config = {
+    params: {
+      service: 'reviews',
+      host: 'localhost',
+      port: 8004,
+      path: '/reviews-products/',
+      productId: id
+    }
+  };
+
+  return axios.get(path, config)
+    .then((response) => {
+      return response.data;
+    });
+}
