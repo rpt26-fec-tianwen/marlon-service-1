40f541b1a27ed0a51cebe4b2d045da6e10d12fab
Marlon Esparza <marlonaesparza@gmail.com>
2021-03-17 17:08:00 -0700

    Logic for rendering stars working

--- client/src/components/CardProductReview.jsx
+++ client/src/components/CardProductReview.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const CardProductReview = (props) => {
+  let stars = [];
+  let rating = 0;
+
+  for (let i = 0; i < rating; i++) {
+    let solidStar = <i class="fas fa-star"></i>;
+    stars.push(solidStar);
+  }
+
+  for (let i = 0; i < Math.abs(rating - 5); i++) {
+    let blankStar = <i class="far fa-star"></i>;
+    stars.push(blankStar);
+  }
+
   return (
     <div className='card-contents-review'>
-      <span>Stars</span>
+      {stars}
     </div>
   );
 };
